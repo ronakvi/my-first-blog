201d581216fdce6c66732cc6c7b7e3830fe51f51
ronak <ronakvi@gmail.com>
2016-08-27 18:58:10 +0000

    My Django Girls app, first commit

---
+++ New_File.py
@@ -0,0 +1,5 @@
+name ='django girls'
+print(name)
+list=[2,3,5634,63]
+list.pop(3)
+print(list)
---
+++ Python_intro.py
@@ -0,0 +1,7 @@
+def hi(name):
+    print('Hi' + ' '+ name +'!' )
+me= 'ronak'
+girls = ['misha', 'ronak', 'dev', 'mrugang', me]
+
+for name in girls:
+   hi(name)
